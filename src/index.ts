5b443ed66dc7ef50a3f09dc8dde8fab4246da1e1
Derrick Hammer <derrick@derrickhammer.com>
2023-04-08 18:03:37 -0400

    * Refactor code to open channel and await for it to be ready.

--- src/index.ts
+++ src/index.ts
@@ -80,9 +80,7 @@ export default class ProtomuxRPC extends EventEmitter {
       onmessage: this._onresponse.bind(this),
     });
 
-    this._channel.open(options?.handshake);
-
-    await this._channel.ready;
+    await this._channel.open(options?.handshake);
   }
 
   _onopen(handshake: any) {
